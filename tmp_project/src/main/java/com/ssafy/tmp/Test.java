ee055177ed0632f3b7db8c54c6b39607e1a1c1ba
yoonoi <dbddbd39@naver.com>
2023-05-15 17:28:46 +0900

    인코딩 수정

--- tmp_project/src/main/java/com/ssafy/tmp/Test.java
+++ tmp_project/src/main/java/com/ssafy/tmp/Test.java
@@ -3,11 +3,7 @@ package com.ssafy.tmp;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("������ Ȯ��");
-		// �����̸� �ּ� ����� commit �Ǵ��� Ȯ�����ּ���
-		System.out.println("������ Ȯ��");
-		System.out.println("���Ǽ� Ȯ��");
-		System.out.println("������ Ȯ��");
+		System.out.println("인코딩 완료");
 	}
 
 }
