1f89fea4dc21b543dd73e83c34bb11a00c91e453
Jung Euiseok <euiseok96@gmail.com>
2023-05-15 17:16:25 +0900

    [+] Mod file JES

--- tmp_project/src/main/java/com/ssafy/tmp/Test.java
+++ tmp_project/src/main/java/com/ssafy/tmp/Test.java
@@ -6,7 +6,7 @@ public class Test {
 		System.out.println("조윤영 확인");
 		// 본인이름 주석 지우고 commit 되는지 확인해주세요
 //		System.out.println("박종민 확인");
-//		System.out.println("정의석 확인");
+		System.out.println("정의석 확인");
 //		System.out.println("이주희 확인");
 	}
 
