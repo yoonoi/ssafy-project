91730272ffdbcf6b1adf9245380f6a70611ef63c
Jy0923 <pbj2y0923@naver.com>
2023-05-15 17:19:47 +0900

    [+]

--- tmp_project/src/main/java/com/ssafy/tmp/Test.java
+++ tmp_project/src/main/java/com/ssafy/tmp/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("조윤영 확인");
 		// 본인이름 주석 지우고 commit 되는지 확인해주세요
-//		System.out.println("박종민 확인");
+		System.out.println("박종민 확인");
 		System.out.println("정의석 확인");
 //		System.out.println("이주희 확인");
 	}
